d0c03bfab2cec910d2f3229bd7cefc447fbc059f
JH074 <00162622@uca.edu.sv>
2025-06-11 10:45:09 -0600

    chore: inicializar proyecto

---
+++ Canchitas/src/test/java/org/ncapas/canchitas/CanchitasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ncapas.canchitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanchitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
